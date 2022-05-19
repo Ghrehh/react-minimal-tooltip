967e0169485a9e1322c671b31eb3fdec46b8dda8
Gerard Burns <gjburns9@hotmail.com>
2022-05-19 16:33:07 +0100

    wip

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,4 @@
-module.exports = [
+/*module.exports = [
   {
     experiments: {
       outputModule: true,
@@ -51,4 +51,66 @@ module.exports = [
       'prop-types'
     ],
   },
+]
+  */
+
+export default [
+  {
+      target: [
+        'web',
+        'es2020'
+      ],
+    experiments: {
+      outputModule: true,
+    },
+    entry: './src/entrypoint.js',
+    output: {
+      filename: 'esm.js',
+      library: {
+        type: "module",
+      },
+    },
+    resolve: {
+      modules: ['src', 'node_modules'],
+    },
+    externals: [
+      'react',
+      'prop-types'
+    ],
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+      ],
+    }
+  },
+  {
+    entry: './src/entrypoint.js',
+    resolve: {
+      modules: ['src', 'node_modules'],
+    },
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: 'babel-loader',
+        },
+      ],
+    },
+    output: {
+      filename: 'cjs.js',
+      library: {
+        name: 'ye',
+        type: "umd"
+      },
+    },
+    externals: [
+      'react',
+      'prop-types'
+    ]
+  }
 ]
